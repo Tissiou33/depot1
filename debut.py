6516c349a42d51f9d68755434476e2b7c683de14
TOSSIM salomon <salomontossim3@gmail.com>
2023-08-20 02:01:24 +0000

    Ma fleur

---
+++ debut.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.speed(0)
+turtle.setup(width=1.0, height=1.0)
+turtle.hideturtle()
+
+couleur = [ "yellow","red","yellow","red"]
+
+for i in range(120):
+    for c in couleur:
+        turtle.color(c)
+        turtle.circle(200-i , 100)
+        turtle.lt(90)
+        turtle.circle(200-i,100)
+        turtle.rt(60)
+        turtle.end_fill
+
+turtle.mainloop()
